2c1de6eb338cc5638d2d5b941211718af5f28543
Aakash-Yadav23 <aakashdev24@gmail.com>
2024-11-14 12:23:00 +0530

    ci added using github actions

---
+++ apps/admin/app/AppWrappers.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { useServerInsertedHTML } from 'next/navigation';
+import { useState } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { ReactNode } from 'react';
+import theme from "@/theme/theme"
+// Create client-side cache
+const createEmotionCache = () => {
+  return createCache({ key: 'css' });
+};
+
+export default function AppWrappers({ children }: { children: ReactNode }) {
+  const [{ cache, flush }] = useState(() => {
+    const cache = createEmotionCache();
+    cache.compat = true;
+    const prevInsert = cache.insert;
+    let inserted: string[] = [];
+    cache.insert = (...args) => {
+      const serialized = args[1];
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name);
+      }
+      return prevInsert.apply(cache, args);
+    };
+    const flush = () => {
+      const prevInserted = inserted;
+      inserted = [];
+      return prevInserted;
+    };
+    return { cache, flush };
+  });
+
+  useServerInsertedHTML(() => {
+    const names = flush();
+    if (names.length === 0) return null;
+    let styles = '';
+    for (const name of names) {
+      styles += cache.inserted[name];
+    }
+    return (
+      <style
+        data-emotion={`${cache.key} ${names.join(' ')}`}
+        dangerouslySetInnerHTML={{
+          __html: styles,
+        }}
+      />
+    );
+  });
+
+  return (
+    <CacheProvider value={cache}>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
