a831b76e962a4fd76ec26654a9d7f1469731cd13
Aakash-Yadav23 <aakashdev24@gmail.com>
2024-11-14 12:43:39 +0530

    feat(commitlint): Add commitlint for better commits

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+// commitlint.config.js
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      // Define custom rules or use default ones here
+      'subject-case': [2, 'always', 'sentence-case'],
+      // Add more rules as needed
+    },
+  };
+  
