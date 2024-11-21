42fb78d92feff548dc55abaa7e00320246f13e20
naman-rathore59 <namanrathore59@gmail.com>
2024-11-21 23:38:01 +0530

    build(add.ts): Create add page ui improvments

--- apps/admin/utils/navigation.ts
+++ apps/admin/utils/navigation.ts
@@ -19,10 +19,10 @@ export const getActiveRoute = (routes: IRoute[]): string => {
   return route?.name || "Default Brand Text";
 };
 
-// export const getActiveNavbar = (routes: IRoute[]): boolean => {
-//   const route = findCurrentRoute(routes);
-//   return route?.secondary;
-// };
+export const getActiveNavbar = (routes: IRoute[]): boolean => {
+  const route = findCurrentRoute(routes);
+  return route?.secondary;
+};
 
 export const getActiveNavbarText = (routes: IRoute[]): string | boolean => {
   return getActiveRoute(routes) || false;
